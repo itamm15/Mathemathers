c6a336f8e3dc8d44a6299b22f47c702b8b1df6b1
itamm15 <osinski.mateusz15@gmail.com>
2025-11-23 21:52:44 +0100

    wip

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class UsersService {
-  construcor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 }
