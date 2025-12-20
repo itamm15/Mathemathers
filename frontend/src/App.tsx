21f674f073f37728c07876b0b59655191dc8f7a9
itamm15 <osinski.mateusz15@gmail.com>
2025-12-20 22:33:04 +0100

    chore: refactor register file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Home from './pages/home';
 import Login from './pages/login';
-import Register from './pages/Register';
+import Register from './pages/register';
 
 type Route = {
   path: string;
