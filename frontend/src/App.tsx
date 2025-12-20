e13862f5ea5dce45569bec797aa09dec366a0c88
itamm15 <osinski.mateusz15@gmail.com>
2025-12-20 22:32:27 +0100

    chore: refactor login file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Home from './pages/home';
-import Login from './pages/Login';
+import Login from './pages/login';
 import Register from './pages/Register';
 
 type Route = {
