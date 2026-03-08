c0aefc55b5ac7dbd311e969ef2e3194e6d6c8bd1
itamm15 <osinski.mateusz15@gmail.com>
2026-03-08 21:17:15 +0100

    chore: add auth modules/services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
