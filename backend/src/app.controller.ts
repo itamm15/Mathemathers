e0f6a345e0aa90353e6d4a74b1bf6bca4f72819b
itamm15 <osinski.mateusz15@gmail.com>
2026-03-09 21:07:05 +0100

    chore(refactor): create local-auth.guard.ts to handle AuthGuard(local)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login') 
   async login(@Request() req) {
     return req.user;
   }
