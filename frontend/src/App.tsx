a6dc3700ba287675a0d2e4bbf40b634c2f7adc42
itamm15 <osinski.mateusz15@gmail.com>
2026-08-02 23:11:35 +0200

    chore: add ProtectedRoute on user required pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
 
-type Route = {
+type AppRoute = {
   path: string;
   element: React.ReactNode;
-}
+};
 
-const LAYOUT_ROUTES: Route[] = [
+const LAYOUT_ROUTES: AppRoute[] = [
   { path: '/', element: <Home /> },
-]
+];
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        {LAYOUT_ROUTES.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}      
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Layout />}>
+          {LAYOUT_ROUTES.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Route>
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
