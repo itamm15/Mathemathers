7351c8d0cfbd3054db08027a80c3b8d1a354ab8e
itamm15 <osinski.mateusz15@gmail.com>
2026-08-22 15:46:25 +0200

    chore: add sooner to project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import { AuthProvider } from './context/AuthContext.tsx';
 import './index.css';
 import App from './App.tsx';
@@ -8,8 +9,9 @@ import App from './App.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>     
+      <AuthProvider>
         <App />
+        <Toaster richColors position="top-right" />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
