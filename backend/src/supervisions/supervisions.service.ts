2acf7102b13947a8c0a5408eecaf3e1f2b17e234
itamm15 <osinski.mateusz15@gmail.com>
2026-09-06 23:00:06 +0200

    chore: define SupervisionsService

---
+++ backend/src/supervisions/supervisions.service.ts
@@ -0,0 +1,55 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { StudentSupervisionStatus } from '@prisma/client';
+import type { InviteSupervisionDto } from '@mathemathers/schemas';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class SupervisionsService {
+  constructor(private prisma: PrismaService) {}
+
+  async invite(supervisorId: string, data: InviteSupervisionDto) {
+    // TODO: Move to user service
+    const supervisor = await this.prisma.user.findUnique({ where: { id: supervisorId } });
+    const student = await this.prisma.user.findUnique({where: { email: data.studentEmail } });
+
+    // Security check, as we don't have triggers yet
+    if (!supervisor || supervisor.role !== 'tutor') {
+      throw new ForbiddenException({ errors: ['SUPERVISOR_NOT_TUTOR'] });
+    }
+
+    if (!student || student.role !== 'student') {
+      throw new NotFoundException({ errors: ['STUDENT_NOT_FOUND'] });
+    }
+
+    // TODO: Move to a private function
+    const existing = await this.prisma.studentSupervision.findUnique({
+      where: {
+        supervisorId_studentId: {
+          supervisorId,
+          studentId: student.id,
+        },
+      },
+    });
+
+    if (existing?.status === StudentSupervisionStatus.PENDING) {
+      throw new BadRequestException({ errors: ['INVITE_PENDING'] });
+    }
+
+    if (existing?.status === StudentSupervisionStatus.ACTIVE) {
+      throw new BadRequestException({ errors: ['ALREADY_LINKED'] });
+    }
+
+    return this.prisma.studentSupervision.create({
+      data: {
+        supervisorId: supervisorId,
+        studentId: student.id,
+        status: StudentSupervisionStatus.PENDING,
+      },
+    });
+  }
+}
